2f9a6ea17b9e42d19141e3d3f04f11e9884d1c20
andreas divaris <dredivaris@gmail.com>
2020-10-23 16:37:07 -0700

    Fix imbibe parsing

--- recipe_scrapers/imbibemagazine.py
+++ recipe_scrapers/imbibemagazine.py
@@ -27,44 +27,86 @@ class ImbibeMagazine(CocktailScraper):
     def _fallback_ingredients(self):
         ingredients = []
         current_ingredient = ''
-        for ingredient in self._get_fallback_content()[1]:
-            if ingredient.name and ingredient.name == 'br':
-                if current_ingredient:
-                    ingredients.append(current_ingredient)
-                    current_ingredient = ''
-            elif ingredient.name and ingredient.name == 'a':
-                current_ingredient += ingredient.get_text()
-            else:
-                l = ingredient.lower()
-                if 'tools:' not in l and 'glass:' not in l and 'garnish:' not in l:
-                    current_ingredient += ingredient.lstrip()
+        try:
+            for ingredient in self._get_fallback_content()[1]:
+                if ingredient.name and ingredient.name == 'br':
+                    if current_ingredient:
+                        ingredients.append(current_ingredient)
+                        current_ingredient = ''
+                elif ingredient.name and ingredient.name == 'a':
+                    current_ingredient += ingredient.get_text()
+                else:
+                    l = ingredient.lower()
+                    if 'tools:' not in l and 'glass:' not in l and 'garnish:' not in l:
+                        current_ingredient += ingredient.lstrip()
+        except:
+            ingredients = []
+            current_ingredient = ''
+            for ingredient in self._get_fallback_content()[2]:
+                if ingredient.name and ingredient.name == 'br':
+                    if current_ingredient:
+                        ingredients.append(current_ingredient)
+                        current_ingredient = ''
+                elif ingredient.name and ingredient.name == 'a':
+                    current_ingredient += ingredient.get_text()
+                else:
+                    l = ingredient.lower()
+                    if 'tools:' not in l and 'glass:' not in l and 'garnish:' not in l:
+                        current_ingredient += ingredient.lstrip()
+            if ingredients:
+                self.is_fallback_v2 = True
         return [normalize_string(i) for i in ingredients]
 
     def _fallback_garnish(self):
-        for ingredient in self._get_fallback_content()[1]:
-            if ingredient.name and ingredient.name == 'br':
-                continue
-            elif ingredient.name and ingredient.name == 'a':
-                continue
-            else:
-                l = ingredient.lower()
-                if 'garnish:' in l:
-                    return normalize_string(l.replace('garnish:', ''.strip()))
+        try:
+            for ingredient in self._get_fallback_content()[1]:
+                if ingredient.name and ingredient.name == 'br':
+                    continue
+                elif ingredient.name and ingredient.name == 'a':
+                    continue
+                else:
+                    l = ingredient.lower()
+                    if 'garnish:' in l:
+                        return normalize_string(l.replace('garnish:', ''.strip()))
+        except:
+            for ingredient in self._get_fallback_content()[2]:
+                if ingredient.name and ingredient.name == 'br':
+                    continue
+                elif ingredient.name and ingredient.name == 'a':
+                    continue
+                else:
+                    l = ingredient.lower()
+                    if 'garnish:' in l:
+                        return normalize_string(l.replace('garnish:', ''.strip()))
+
         return ''
 
     def _fallback_glass(self):
-        for ingredient in self._get_fallback_content()[1]:
-            if ingredient.name and ingredient.name == 'br':
-                continue
-            elif ingredient.name and ingredient.name == 'a':
-                continue
-            else:
-                l = ingredient.lower()
-                if 'glass:' in l:
-                    return normalize_string(l.replace('glass:', ''.strip()))
+        try:
+            for ingredient in self._get_fallback_content()[1]:
+                if ingredient.name and ingredient.name == 'br':
+                    continue
+                elif ingredient.name and ingredient.name == 'a':
+                    continue
+                else:
+                    l = ingredient.lower()
+                    if 'glass:' in l:
+                        return normalize_string(l.replace('glass:', ''.strip()))
+        except:
+            for ingredient in self._get_fallback_content()[2]:
+                if ingredient.name and ingredient.name == 'br':
+                    continue
+                elif ingredient.name and ingredient.name == 'a':
+                    continue
+                else:
+                    l = ingredient.lower()
+                    if 'glass:' in l:
+                        return normalize_string(l.replace('glass:', ''.strip()))
+
         return ''
 
     def ingredients(self):
+
         try:
             ingredients_html = self.soup.findAll('div', {'class': ['single-box', 'clearfix', 'entry-content']})[1].findAll('p')[1]
         except IndexError:
@@ -100,13 +142,20 @@ class ImbibeMagazine(CocktailScraper):
 
     def instructions(self):
         try:
-            instructions = self.soup.findAll('div', {'class': ['single-box', 'clearfix', 'entry-content']})[1].findAll('p')[2].get_text()
+            instructions = self.soup\
+            .findAll('div', {'class': ['single-box', 'clearfix', 'entry-content']})[1]\
+            .findAll('p')[2].get_text()
         except IndexError:
             try:
-                instructions = self.soup.findAll('div', {'class': ['preparation__content']})[0].get_text().strip()
+                instructions = self.soup.findAll(
+                    'div', {'class': ['preparation__content']})[0].get_text().strip()
             except IndexError:
-                return self._get_fallback_content()[2].get_text()
-        return instructions
+                self.ingredients()
+                if getattr(self, 'is_fallback_v2', False):
+                    return normalize_string(self._get_fallback_content()[3].get_text())
+                else:
+                    return normalize_string(self._get_fallback_content()[2].get_text())
+        return normalize_string(instructions)
 
     def glass(self):
         try:
